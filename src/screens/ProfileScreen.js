4864cbda645dff1718261469940e5e8d2476ca15
Shantanu Aggarwal <shantanu241099believer@gmail.com>
2023-07-14 10:29:47 +0530

    Update ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+// for profile screen
 const ProfileScreen = () => {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
